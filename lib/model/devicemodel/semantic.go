92ea22925c72ee193b5c201eed6ad583ef16393c
Ingo <roessner@infai.org>
2019-09-05 15:40:50 +0200

    update device model

--- lib/model/devicemodel/semantic.go
+++ lib/model/devicemodel/semantic.go
@@ -36,19 +36,19 @@ type Aspect struct {
 }
 
 type Concept struct {
-	Id              string           `json:"id"`
-	Name            string           `json:"name"`
-	Characteristics []Characteristic `json:"characteristics"`
-	RdfType         string           `json:"rdf_type"`
+	Id                string   `json:"id"`
+	Name              string   `json:"name"`
+	CharacteristicIds []string `json:"characteristic_ids"`
+	RdfType           string   `json:"rdf_type"`
 }
 
 type Characteristic struct {
 	Id                 string           `json:"id"`
 	Name               string           `json:"name"`
 	Type               Type             `json:"type"`
-	MinValue           float64          `json:"min_value"`
-	MaxValue           float64          `json:"max_value"`
-	Value              interface{}      `json:"value"`
+	MinValue           interface{}      `json:"min_value,omitempty"`
+	MaxValue           interface{}      `json:"max_value,omitempty"`
+	Value              interface{}      `json:"value,omitempty"`
 	SubCharacteristics []Characteristic `json:"sub_characteristics"`
 	RdfType            string           `json:"rdf_type"`
 }
