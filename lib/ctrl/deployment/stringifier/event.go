213977827e119b24a1c942641577813ecb9ff0a2
Ingo <roessner@infai.org>
2020-07-17 12:36:46 +0200

    SNRGY-126 v2 implement event stringifier

--- lib/ctrl/deployment/stringifier/event.go
+++ lib/ctrl/deployment/stringifier/event.go
@@ -17,14 +17,50 @@
 package stringifier
 
 import (
+	"errors"
+	"fmt"
 	"github.com/SENERGY-Platform/process-deployment/lib/model/deploymentmodel/v2"
 	"github.com/beevik/etree"
+	uuid "github.com/satori/go.uuid"
+	"log"
+	"runtime/debug"
+	"strings"
 )
 
-func (this *Stringifier) MessageEvent(doc *etree.Document, element deploymentmodel.Element) error {
-	panic("not implemented")
+func (this *Stringifier) MessageEvent(doc *etree.Document, element deploymentmodel.Element) (err error) {
+	defer func() {
+		if r := recover(); r != nil && err == nil {
+			log.Printf("%s: %s", r, debug.Stack())
+			err = errors.New(fmt.Sprint("Recovered Error: ", r))
+		}
+	}()
+	if element.MessageEvent.EventId == "" {
+		element.MessageEvent.EventId = "generated_" + uuid.NewV4().String()
+	}
+	msgRef := strings.Replace("generated_ref_"+element.MessageEvent.EventId, "-", "_", -1)
+	bpmnMsg := doc.CreateElement("bpmn:message")
+	doc.SelectElement("bpmn:definitions").InsertChildAt(doc.SelectElement("bpmn:definitions").SelectElement("bpmndi:BPMNDiagram").Index(), bpmnMsg)
+	bpmnMsg.CreateAttr("id", msgRef)
+	bpmnMsg.CreateAttr("name", element.MessageEvent.EventId)
+	doc.FindElement("//*[@id='"+element.BpmnId+"']/bpmn:messageEventDefinition").CreateAttr("messageRef", msgRef)
+	return nil
 }
 
-func (this *Stringifier) TimeEvent(doc *etree.Document, element deploymentmodel.Element) error {
-	panic("not implemented")
+func (this *Stringifier) TimeEvent(doc *etree.Document, element deploymentmodel.Element) (err error) {
+	defer func() {
+		if r := recover(); r != nil && err == nil {
+			log.Printf("%s: %s", r, debug.Stack())
+			err = errors.New(fmt.Sprint("Recovered Error: ", r))
+		}
+	}()
+	timeDefinition := doc.FindElement("//*[@id='" + element.BpmnId + "']/bpmn:timerEventDefinition")
+	if definitionElement := timeDefinition.FindElement("./bpmn:" + element.TimeEvent.Type); definitionElement != nil {
+		definitionElement.SetText(element.TimeEvent.Time)
+	} else {
+		definitionElement = doc.CreateElement("bpmn:" + element.TimeEvent.Type)
+		definitionElement.CreateAttr("xsi:type", "bpmn:tFormalExpression")
+		definitionElement.SetText(element.TimeEvent.Time)
+		timeDefinition.InsertChildAt(0, definitionElement)
+	}
+	return nil
 }
